ee088f3aa58f632f25299141b91275b6587ba522
LuQmu5 <luqmu5.1007@gmail.com>
2025-06-23 13:57:57 +0400

    jump and camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using Unity.Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class PlayerController : MonoBehaviour
     [Header("Jump")]
     [SerializeField] private float _jumpDuration = 1f;
     [SerializeField] private AnimationCurve _jumpCurve;
+    [SerializeField] private float _jumpForwardMultiplier = 2f;
+
+    [Header("Camera Effects")]
+    [SerializeField] private CinemachineCamera _cinemachineCamera;
+    [SerializeField] private float _jumpFovBoost = 10f;
+    [SerializeField] private Vector3 _jumpOffsetBoost = new Vector3(0f, 0.5f, -1f);
 
     [Header("References")]
     [SerializeField] private PlayerInput _input;
@@ -18,8 +25,11 @@ public class PlayerController : MonoBehaviour
 
     private float _targetX;
     private bool _isJumping = false;
-
     private float _horizontalInput = 0f;
+
+    private float _defaultFov;
+    private Vector3 _defaultOffset;
+    private CinemachineFollow _composer;
 
     private void OnEnable()
     {
@@ -36,6 +46,15 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         _targetX = transform.position.x;
+
+        if (_cinemachineCamera != null)
+        {
+            _defaultFov = _cinemachineCamera.Lens.FieldOfView;
+            _composer = _cinemachineCamera.GetComponent<CinemachineFollow>();
+
+            if (_composer != null)
+                _defaultOffset = _composer.FollowOffset;
+        }
     }
 
     private void Update()
@@ -44,7 +63,6 @@ public class PlayerController : MonoBehaviour
         {
             _targetX += _horizontalInput * _sideSpeed * Time.deltaTime;
             _targetX = Mathf.Clamp(_targetX, -_laneLimit, _laneLimit);
-
             UpdateMovement();
         }
         else
@@ -86,22 +104,58 @@ public class PlayerController : MonoBehaviour
         _isJumping = true;
         _view.SetJump(true);
 
+        float camTransitionTime = 0.2f;
+        float elapsed = 0f;
+
+        float startFov = _cinemachineCamera.Lens.FieldOfView;
+        float targetFov = _defaultFov + _jumpFovBoost;
+
+        Vector3 startOffset = _composer.FollowOffset;
+        Vector3 targetOffset = _defaultOffset + _jumpOffsetBoost;
+
+        while (elapsed < camTransitionTime)
+        {
+            float t = elapsed / camTransitionTime;
+            _cinemachineCamera.Lens.FieldOfView = Mathf.Lerp(startFov, targetFov, t);
+            _composer.FollowOffset = Vector3.Lerp(startOffset, targetOffset, t);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        _cinemachineCamera.Lens.FieldOfView = targetFov;
+        _composer.FollowOffset = targetOffset;
+
+        // Ïðûæîê
         float time = 0f;
         Vector3 startPos = transform.position;
 
         while (time < _jumpDuration)
         {
-            float curveY = _jumpCurve.Evaluate(time / _jumpDuration);
+            float normalizedTime = time / _jumpDuration;
+            float curveY = _jumpCurve.Evaluate(normalizedTime);
             float y = startPos.y + curveY;
-            float z = startPos.z + _forwardSpeed * time;
+            float z = startPos.z + _forwardSpeed * _jumpForwardMultiplier * normalizedTime;
             transform.position = new Vector3(startPos.x, y, z);
-
             time += Time.deltaTime;
             yield return null;
         }
 
-        Vector3 endPos = new Vector3(startPos.x, startPos.y, startPos.z + _forwardSpeed * _jumpDuration);
-        transform.position = endPos;
+        float finalZ = startPos.z + _forwardSpeed * _jumpForwardMultiplier;
+        transform.position = new Vector3(startPos.x, startPos.y, finalZ);
+
+        elapsed = 0f;
+
+        while (elapsed < camTransitionTime)
+        {
+            float t = elapsed / camTransitionTime;
+            _cinemachineCamera.Lens.FieldOfView = Mathf.Lerp(targetFov, _defaultFov, t);
+            _composer.FollowOffset = Vector3.Lerp(targetOffset, _defaultOffset, t);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        _cinemachineCamera.Lens.FieldOfView = _defaultFov;
+        _composer.FollowOffset = _defaultOffset;
 
         _isJumping = false;
         _view.SetJump(false);
